c97ae9a47a5288959135ac8170ac223aabbc29bf
Alaa Samir <eng.alaa2023@gmail.com>
2025-02-08 12:27:28 +0200

    updates

--- src/models/enums/ResponeEnums.py
+++ src/models/enums/ResponeEnums.py
@@ -6,4 +6,6 @@ class ResponseSignal(Enum):
     FILE_SIZE_EXCEEDED="file_size_exceeded"
     FILE_UPLOADED_SUCCESS="file_uploaded_success"
     FILE_UPLOADED_FAILED="file_uploaded_failed"
+    PROCESSING_SUCCESS="processing_success"
+    PROCESSING_FAILED="processing_faild"
    
